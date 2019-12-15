26180aaf1eaa72c8b795700b06ad92b14fe139c8
naomipappe <naomipappe.zvieriev@gmail.com>
2019-12-15 14:55:04 +0200

    heat equation sort of completed

--- heat-eq/main.py
+++ heat-eq/main.py
@@ -1,88 +1,111 @@
 import numpy as np
-
-
-K_C_DIFF_ABS = 273.15
+import tabulate as tb
+KELVIN_CELCIUS_DELTA = 273.15
+# region task specific constants and functions
+m = 2
 R = 0.15
-rho = 790
+lambd = 45.5
 c = 460
+rho = 790
 gamma = 140
-lambd = 42.1
-u_env = 20 + K_C_DIFF_ABS
+temp_env = 20 + KELVIN_CELCIUS_DELTA
 T = 20*60
 
 
-alpha = lambd/(c*rho)
-T1 = alpha*T/(R**2)
-gamma1 = R/lambd * gamma
+def initial_temp(x: float) -> float:
+    return 100 + 20*np.sin(x*(R-x)) + KELVIN_CELCIUS_DELTA
 
 
-def u0(x: float) -> float:
-    return 100 + 20 * np.sin(x*(R-x)) + K_C_DIFF_ABS
+# endregion
 
 
-def v0(x: float) -> float:
-    return (u0(x)-u_env)/u_env
+# region shapeless constants and function
+a_squared = lambd/(c*rho)
+T1 = a_squared * T/(R**2)
+gamma1 = gamma*R/(lambd)
 
 
-def to_celcius(temp: float) -> float:
-    return temp*u_env+u_env-K_C_DIFF_ABS
+def initial_temp_shapeless(x: float) -> float:
+    return (initial_temp(x) - temp_env)/temp_env
+
+# endregion
 
 
-N = 10
-M = 200
-sigm = 0.5
-tau, h = T1/M, 1/N
-print('Tau =', tau)
-print('h =', h)
+def convert_temperature(temperature: float) -> float:
+    return temperature*temp_env+temp_env-KELVIN_CELCIUS_DELTA
+
+
+def print_converted(temperatures):
+    print([convert_temperature(temp) for temp in temperatures])
+
+
+alpha1 = 1
+alpha2 = 1
+beta1 = mu1 = 0
+beta2 = gamma1
+mu2 = 0
+
+h = 0.1
+tau = 0.1
+N = int(np.ceil(1/h))
+M = int(np.ceil(T1/tau))
+sigma = 0.5
 
 x = [i*h for i in range(N+1)]
 
 dx = np.zeros_like(x)
-dx[0] = (x[1]**3)/(3*h)
-dx[N] = (x[N]**3-x[N-1]**3)/(3*h)
-
+dx[0] = (x[1]**(m + 1)-x[0]**(m + 1)) / ((m + 1)*h)
+dx[N] = (x[N]**(m + 1)-x[N-1]**(m + 1)) / ((m + 1)*h)
 for i in range(1, N):
-    dx[i] = (x[i+1]**3-x[i-1]**3)/(6*h)
+    dx[i] = (x[i]**(m + 1)-x[i-1]**(m + 1)) / ((m + 1)*2*h)
 
-dp = list()
+dp = np.zeros(N)
+for i in range(len(dp)):
+    dp[i] = (x[i+1]-h/2)**2
 
-for i in range(1, N+1):
-    dp.append((x[i] - h/2)**2)
-
-dp = np.array(dp)
-input()
-y = [v0(node) for node in x]
-print([to_celcius(temp) for temp in y])
-for j in range(1, M+1):
-    A = np.zeros((N+1, N+1))
-    yy = np.zeros_like(x)
-
-    b = sigm*tau/(h**2)*dp[0]
-    c = -1/2*dx[0]-b
-    phi = -1/2*dx[0]*y[0] - (1 - sigm)*tau/(h**2)*dp[0]*(y[1] - y[0])
-
+y = [initial_temp_shapeless(point) for point in x]
+print("Initial temperature distribution:",
+      [convert_temperature(temp) for temp in y], sep='\n')
+table = []
+table.append([0] + [convert_temperature(temp) for temp in y])
+for j in range(M):
+    time = (j+1) * T/M
+    # region construct a scheme
+    A = np.zeros((N + 1, N + 1))
+    yy = np.zeros(N + 1)
+    b = tau*sigma/(h**2)*dp[0]
+    c = - b - dx[0]/2
+    phi = -(1-sigma)*tau/(h**2)*dp[0]*(y[1]-y[0])-dx[0]*y[0]/2
     A[0][0] = c
     A[0][1] = b
     yy[0] = phi
 
     for i in range(1, N):
-        d = sigm*tau/(h**2)*dp[i-1]
-        b = sigm*tau/(h**2)*dp[i]
-        c = -dx[i] - (b + d)
-        phi = -1/2*dx[0]*y[0]-(1-sigm)*tau/(h**2) * \
-            (dp[i]*(y[i+1]-y[i])-dp[i-1]*(y[i]-y[i-1]))
-        A[i][i] = c
+        b = tau*sigma/(h**2)*dp[i]
+        d = tau*sigma/(h**2)*dp[i-1]
+        c = -dx[i] - (d + b)
+        phi = -dx[i]*y[i]-tau*(1-sigma)/(h**2) * \
+            (dp[i]*(y[i+1] - y[i]) - dp[i-1]*(y[i] - y[i-1]))
         A[i][i-1] = d
+        A[i][i] = c
         A[i][i+1] = b
         yy[i] = phi
-    d = 0
-    c - sigm*tau/h*gamma1*x[N]-1/2*dx[N] - d
-    phi = (1-sigm)*tau/h*gamma1*x[N]*y[N] #- tau/h*x[N]*gamma1*u_env
+
+    d = tau*sigma/(h**2)*dp[N-1]
+    c = -d - sigma*tau/h*x[N]*beta2-dx[N]/2
+    phi = (1-sigma)*tau/(h**2)*dp[N-1]*(y[N]-y[N-1])+tau / \
+        h*(1-sigma)*x[N]*beta2*y[N]-tau/h*x[N]*mu2-dx[N]*y[N]/2
 
     A[N][N-1] = d
     A[N][N] = c
     yy[N] = phi
-    y = np.linalg.solve(A, yy)
-    print([to_celcius(temp) for temp in y])
 
-print([to_celcius(temp) for temp in y])
+    y = np.linalg.solve(A, yy)
+    table.append([time/60]+[convert_temperature(temp) for temp in y])
+    # print_converted(y)
+    # endregion
+
+head = ["Time"]+["{0:.2f}".format(point*R) for point in x]
+with open(r'.\heat.txt', '+w') as outfile:
+    for line in tb.tabulate(table, headers=head):
+        outfile.write(line)
