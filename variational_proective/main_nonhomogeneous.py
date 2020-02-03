5b055491d06235dd75ddd0826cef9d3db3d0fe06
Ihor Zvieriev <naomipappe.zvieriev@gmail.com>
2020-02-03 23:10:51 +0200

    Minor refactoring of variational - proective methods (#1)
    * minor refactoring of FunctionalSystem "class"
    
    * Initial refactoring of approx methods.py
    
    * minor refactoring of FunctionalSystem "class"
    
    * minor refactoring of calling function
    
    * Initial Ritz method refactoring
    
    * Extracted approximation building
    
    * Ritz method refactoring successful
    
    * Bubnov - Galerkin method refactoring successful
    
    * Minor refactoring completed, methods.py look more or less sane

---
+++ variational_proective/main_nonhomogeneous.py
@@ -0,0 +1,108 @@
+from numpy import linspace
+from sympy import lambdify
+from sympy.abc import symbols
+from sympy.parsing.sympy_parser import (parse_expr, standard_transformations, implicit_multiplication, )
+
+from functional.fun_sys import BasisFunction
+from methods.methods import Ritz, BubnovGalerkin, LeastSquares
+from utilities.util import plotter
+
+transformations = standard_transformations + (implicit_multiplication,)
+variable = symbols("x")
+
+# region assignment specific constants
+
+BORDER_LEFT, BORDER_RIGHT = 1, 2
+m1, m2, m3 = 1, 2, 1
+p1, p2, p3 = 2, 1, 2
+q1, q2, q3 = 1, 1, 1
+k1, k2, k3 = 1, 1, 1
+beta = -6
+delta = 3
+# endregion
+# region assignment specific functions
+solution_exact_expression = parse_expr(f'{m1}*sin({m2}*x)+{m3}', evaluate=True)
+solution_exact_expression_dx = solution_exact_expression.diff(variable)
+solution_exact_expression_d2x = solution_exact_expression.diff(variable, 2)
+
+k_expression = parse_expr(f'{k1}*(x**{k2})+{k3}', evaluate=True)
+k_expression_dx = k_expression.diff(variable)
+
+p_expression = parse_expr(f'{p1} * (x ** {p2}) + {p3}', evaluate=True)
+
+q_expression = parse_expr(f'{q1} * (x ** {q2}) + {q3}', evaluate=True)
+
+
+def solution_exact(x: float) -> float:
+    return lambdify(variable, solution_exact_expression, "numpy")(x)
+
+
+def solution_exact_dx(x: float) -> float:
+    return lambdify(variable, solution_exact_expression_dx, "numpy")(x)
+
+
+def solution_exact_d2x(x: float) -> float:
+    return lambdify(variable, solution_exact_expression_d2x, "numpy")(x)
+
+
+def k(x: float) -> float:
+    return lambdify(variable, k_expression, "numpy")(x)
+
+
+def dk(x: float) -> float:
+    return lambdify(variable, k_expression_dx, "numpy")(x)
+
+
+def p(x: float) -> float:
+    return lambdify(variable, p_expression, "numpy")(x)
+
+
+def q(x: float) -> float:
+    return lambdify(variable, q_expression, "numpy")(x)
+
+
+def mu_1() -> float:
+    return -k(BORDER_LEFT) * solution_exact_dx(BORDER_LEFT) + beta * solution_exact(BORDER_LEFT)
+
+
+def mu_2() -> float:
+    return k(BORDER_RIGHT) * solution_exact_dx(BORDER_RIGHT) + delta * solution_exact(BORDER_RIGHT)
+
+
+def differential_operator(u):
+    return (-k_expression * u.diff(variable)).diff(variable) + p_expression * u.diff(variable) + \
+           q_expression * u
+
+# endregion
+
+
+def main():
+    alpha = -k(BORDER_LEFT)
+    gamma = k(BORDER_RIGHT)
+    nodes = linspace(BORDER_LEFT, BORDER_RIGHT, 50, endpoint=True)
+    # region Ritz
+    n_Ritz = 10
+    functional_system = BasisFunction((BORDER_LEFT, BORDER_RIGHT), alpha, beta, gamma, delta, k, mu_1, mu_2, variable)
+    Ritz.set_functional_system(functional_system)
+
+    approximation_ritz, error_ritz = Ritz.approximation(n_Ritz, differential_operator(solution_exact_expression),
+                                                        differential_operator)
+    plotter(nodes, solution_exact, approximation_ritz, save=False)
+    print("Вектор невязки(Метод Ритца):", error_ritz)
+    # endregion
+
+    # region Bubnov
+    n_Bubnov = 10
+
+    BubnovGalerkin.set_functional_system(functional_system)
+
+    approximation_bubnov, error_bubnov = BubnovGalerkin.approximation(n_Bubnov,
+                                                                      differential_operator(solution_exact_expression),
+                                                                      differential_operator)
+    print("Вектор невязки(Метод Бубнова - Галёркина):", error_bubnov)
+    plotter(nodes, solution_exact, approximation_bubnov, save=False)
+    # endregion
+
+
+if __name__ == "__main__":
+    main()
---
+++ variational_proective/methods/methods.py
@@ -0,0 +1,160 @@
+from typing import Type, Tuple, Callable
+
+import numpy as np
+from scipy import integrate
+from sympy import lambdify
+
+from variational_proective.functional.fun_sys import FunctionalSystem
+
+
+class VariationalProective:
+    _functional_system: Type[FunctionalSystem] = None
+
+    @classmethod
+    def approximation(cls, n: int, equation_rhs, differential_operator):
+        __coefficients, error = cls.coefficients(n, equation_rhs, differential_operator)
+
+        def f(x):
+            return lambdify(
+                cls._functional_system.variable,
+                cls._functional_system.get_basic_zero() + sum(
+                    [__coefficients[i] * cls._functional_system.get_function(i)
+                     for i in range(n)]), 'numpy')(x)
+
+        return f, error
+
+    @classmethod
+    def coefficients(cls, n, equation_rhs, differential_operator) -> Tuple[Callable[[float], float], list]:
+        raise NotImplementedError
+
+    @classmethod
+    def set_functional_system(cls, functional_system: Type[FunctionalSystem]):
+        cls._functional_system = functional_system
+
+
+class Ritz(VariationalProective):
+    @classmethod
+    def coefficients(cls, n, equation_rhs, differential_operator):
+        matrix, vector = cls.__build_system(n, equation_rhs, differential_operator)
+        _coefficients = np.linalg.solve(matrix, vector)
+        error = vector - np.dot(matrix, _coefficients)
+        return _coefficients, error
+
+    @classmethod
+    def __build_system(cls, n: int, equation_rhs, differential_operator):
+        def rhs_homogeneous():
+            return equation_rhs - differential_operator(cls._functional_system.get_basic_zero())
+
+        def matrix_element(i: int, j: int) -> float:
+            return integrate.quad(lambdify(cls._functional_system.variable, differential_operator(
+                cls._functional_system.get_function(i)) * cls._functional_system.get_function(j), "numpy", ),
+                                  *cls._functional_system.borders)[0]
+
+        def vector_element(i: int) -> float:
+            return integrate.quad(
+                lambdify(cls._functional_system.variable, rhs_homogeneous() * cls._functional_system.get_function(i),
+                         "numpy"), *cls._functional_system.borders)[0]
+
+        matrix = np.array(
+            [[matrix_element(i, j) for i in range(n)] for j in range(n)]
+        )
+
+        vector = np.array([vector_element(i) for i in range(n)])
+
+        return matrix, vector
+
+
+class Collocation(VariationalProective):
+    __nodes = None
+
+    @classmethod
+    def coefficients(cls, n: int, equation_rhs, differential_operator):
+        matrix, vector = cls.__build_system(n, equation_rhs, differential_operator)
+        _coefficients = np.linalg.solve(matrix, vector)
+        error = vector - np.dot(matrix, _coefficients)
+        return _coefficients, error
+
+    @classmethod
+    def __build_system(cls, n: int, equation_rhs, differential_operator):
+        def rhs_homogeneous(x: float) -> float:
+            return lambdify(cls._functional_system.variable,
+                            equation_rhs - differential_operator(cls._functional_system.get_basic_zero()),
+                            'numpy')(x)
+
+        def make_matrix_element(i: int, j: int) -> float:
+            return lambdify(cls._functional_system.variable,
+                            differential_operator(cls._functional_system.get_function(j)), 'numpy')(cls.__nodes[i])
+
+        cls.__nodes = np.linspace(*cls._functional_system.borders, n, endpoint=True)
+
+        matrix = np.array(
+            [[make_matrix_element(i, j) for j in range(len(cls.__nodes))] for i in range(len(cls.__nodes))]
+        )
+        vector = np.array([rhs_homogeneous(cls.__nodes[i]) for i in range(n)])
+
+        return matrix, vector
+
+
+class LeastSquares(VariationalProective):
+    @classmethod
+    def coefficients(cls, n, equation_rhs, differential_operator) -> Tuple[Callable[[float], float], list]:
+        matrix, vector = cls.__build_system(n, equation_rhs, differential_operator)
+        _coefficients = np.linalg.solve(matrix, vector)
+        error = vector - np.dot(matrix, _coefficients)
+        return _coefficients, error
+
+    @classmethod
+    def __build_system(cls, n: int, equation_rhs, differential_operator):
+        def rhs_homogeneous():
+            return equation_rhs - differential_operator(cls._functional_system.get_basic_zero())
+
+        def make_matrix_element(i: int, j: int) -> float:
+            return integrate.quad(lambdify(cls._functional_system.variable,
+                                           differential_operator(
+                                               cls._functional_system.get_function(i)) *
+                                           differential_operator(
+                                               cls._functional_system.get_function(j)), "numpy"),
+                                  *cls._functional_system.borders)[0]
+
+        def make_vector_element(j: int) -> float:
+            return integrate.quad(lambdify(cls._functional_system.variable,
+                                           rhs_homogeneous() * differential_operator(
+                                               cls._functional_system.get_function(j)),
+                                           'numpy'), *cls._functional_system.borders)[0]
+
+        matrix = np.array([[make_matrix_element(i, j) for i in range(n)] for j in range(n)])
+        vector = np.array([make_vector_element(j) for j in range(n)])
+        print(matrix)
+        return matrix, vector
+
+
+class BubnovGalerkin(VariationalProective):
+    @classmethod
+    def coefficients(cls, n, equation_rhs, differential_operator) -> Tuple[Callable[[float], float], list]:
+        matrix, vector = cls.__build_system(n, equation_rhs, differential_operator)
+        __coefficients = np.linalg.solve(matrix, vector)
+        error = vector - np.dot(matrix, __coefficients)
+        return __coefficients, error
+
+    @classmethod
+    def __build_system(cls, n: int, equation_rhs, differential_operator):
+        def rhs_homogeneous():
+            return equation_rhs - differential_operator(cls._functional_system.get_basic_zero())
+
+        def make_matrix_element(i: int, j: int):
+            return integrate.quad(lambdify(
+                cls._functional_system.variable,
+                differential_operator(cls._functional_system.get_function(i)) * cls._functional_system.get_function(j),
+                "numpy"), *cls._functional_system.borders)[0]
+
+        def make_vector_element(i: int):
+            return integrate.quad(
+                lambdify(cls._functional_system.variable, rhs_homogeneous() * cls._functional_system.get_function(i),
+                         "numpy"), *cls._functional_system.borders)[0]
+
+        matrix = np.array(
+            [[make_matrix_element(i, j) for i in range(n)] for j in range(n)]
+        )
+        vector = np.array([make_vector_element(i) for i in range(n)])
+
+        return matrix, vector
