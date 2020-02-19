75b1b82b730673ed5cfee0ebaecb17573315fe25
Ihor Zvieriev <naomipappe.zvieriev@gmail.com>
2020-02-19 22:52:08 +0200

    Refactoring diffschemes (#4)
    * Minor refactoring of IntegroInterpolation - 1 method, brute solution for p(x) function integral approximation used
    
    * Initial impl of monotonic scheme with regard to sign of p(x) function

--- diff-scheme/scheme/diff_scheme.py
+++ diff-scheme/scheme/diff_scheme.py
@@ -22,10 +22,10 @@ class FiniteElementDiffScheme:
 
     def solve(self, n: int):
         matrix, vector = self.__build_system(n)
-        coeficients = np.linalg.solve(matrix, vector)
+        coefficients = np.linalg.solve(matrix, vector)
 
         def approximation(x: float):
-            return sum([coeficients[i]*self.__phi(i, x) for i in range(n+1)])
+            return sum([coefficients[i] * self.__phi(i, x) for i in range(n + 1)])
         return approximation
 
     def __build_system(self, n: int):
@@ -150,6 +150,88 @@ class IntegroInterpolationScheme:
             return integrate.quad(self._p, self._nodes[i] - self._step / 2, self._nodes[i])[0]
         else:
             return integrate.quad(self._p, self._nodes[i] - self._step / 2, self._nodes[i] + self._step / 2)[0]
+
+    def phi(self, i: int, equation_rhs):
+        if i == 0:
+            return 2 / self._step * integrate.quad(equation_rhs, self._nodes[i], self._nodes[i] + self._step / 2)[0]
+        elif i == len(self._nodes) - 1:
+            return 2 / self._step * integrate.quad(equation_rhs, self._nodes[i] - self._step / 2, self._nodes[i])[0]
+        else:
+            return 1 / self._step * \
+                   integrate.quad(equation_rhs, self._nodes[i] - self._step / 2, self._nodes[i] + self._step / 2)[0]
+
+
+class MonotonicScheme:
+    def __init__(self, borders: tuple, alpha: float, beta: float, gamma: float, delta: float, mu_1, mu_2, k, p, q):
+        self._nodes, self._step = None, None
+        self._left_border, self._right_border = borders
+        self._k = k
+        self._p = p
+        self._q = q
+        self._mu_1 = mu_1
+        self._mu_2 = mu_2
+        self._alpha = alpha
+        self._beta = beta
+        self._delta = delta
+        self._gamma = gamma
+
+    def solve(self, n: int, equation_rhs):
+        matrix, vector = self._build_system(n, equation_rhs)
+        matrix, vector = self._build_system(n, equation_rhs)
+        results = np.linalg.solve(matrix, vector)
+        print(vector - np.dot(matrix, results))
+        return results
+
+    def _build_system(self, n, equation_rhs):
+        def call_equation_rhs(x: float) -> float:
+            return lambdify('x', equation_rhs, 'numpy')(x)
+
+        self._nodes, self._step = np.linspace(self._left_border, self._right_border, n, endpoint=True, retstep=True)
+        matrix = np.zeros((n, n))
+        vector = np.zeros(n)
+
+        matrix[0][0] = self.a(1) / self._step + self._beta + self._step / 2 * self.d(0)  # - self.s(0) / self._step
+        matrix[0][1] = -self.a(1) / self._step  # + self.s(0) / self._step
+
+        matrix[n - 1][n - 2] = -self.a(n - 1) / self._step  # - self.s(n - 1) / self._step
+        matrix[n - 1][n - 1] = self._delta + self.a(n - 1) / self._step + self._step / 2 * self.d(n - 1)  # + self.s(
+        # n - 1) / self._step
+        for i in range(1, n - 1):
+            matrix[i][i - 1] = -self.a(i) / (self._step ** 2) - self.b_neg(i) * self.a(i) / self._step
+            matrix[i][i] = (self.a(i + 1) + self.a(i)) / (self._step ** 2) - (
+                    self.b_pos(i) * self.a(i + 1) + self.b_neg(i) * self.a(i)) / self._step + self.d(i)
+            matrix[i][i + 1] = -self.a(i + 1) / (self._step ** 2) + self.b_pos(i) * self.a(i + 1) / self._step
+
+        vector[0] = (self._step / 2) * self.phi(0, call_equation_rhs) + self._mu_1()
+        vector[n - 1] = (self._step / 2) * self.phi(n - 1, call_equation_rhs) + self._mu_2()
+        for i in range(1, n - 1):
+            vector[i] = self.phi(i, call_equation_rhs)
+        return matrix, vector
+
+    def d(self, i: int) -> float:
+        if i == 0:
+            return 2 / self._step * integrate.quad(self._q, self._nodes[i], self._nodes[i] + self._step / 2)[0]
+        elif i == len(self._nodes) - 1:
+            return 2 / self._step * integrate.quad(self._q, self._nodes[i] - self._step / 2, self._nodes[i])[0]
+        else:
+            return 1 / self._step * \
+                   integrate.quad(self._q, self._nodes[i] - self._step / 2, self._nodes[i] + self._step / 2)[0]
+
+    def a(self, i: int) -> float:
+        return (1 / self._step * integrate.quad(lambda x: 1 / self._k(x), self._nodes[i - 1], self._nodes[i])[0]) ** (
+            -1)
+
+    def b_pos(self, i: int) -> float:
+        return self._r_pos(i) / self._k(self._nodes[i])
+
+    def b_neg(self, i: int) -> float:
+        return self._r_neg(i) / self._k(self._nodes[i])
+
+    def _r_pos(self, i: int) -> float:
+        return (self._p(self._nodes[i]) + abs(self._p(self._nodes[i]))) / 2
+
+    def _r_neg(self, i: int) -> float:
+        return (self._p(self._nodes[i]) - abs(self._p(self._nodes[i]))) / 2
 
     def phi(self, i: int, equation_rhs):
         if i == 0:
