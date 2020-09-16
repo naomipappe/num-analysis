76336b22743dfa823b30a3db28f46fcf8d02d30d
naomipappe <naomipappe.zvieriev@gmail.com>
2020-09-17 02:09:20 +0300

    Refactored Non Linear Equations solutions

--- nonlineareqshowcase.py
+++ nonlineareqshowcase.py
@@ -1,4 +1,5 @@
-from numanalysis.nonlineareq.non_linear_equations import secant
+from numpy.testing._private.utils import assert_equal
+from numanalysis.nonlineareq.non_linear_equations import *
 from numpy import sin, cos
 
 
@@ -14,4 +15,10 @@ def lhs_derr_2(x: float) -> float:
     return -sin(x+2)-2
 
 
-print(f"Equation root is : {secant(lhs,1,2,1,1.5)}")
+left_border, right_border = 1, 2
+initial_root_candidate = 1
+
+equation = NonLinearEquation(lhs, left_border, right_border, 1, lhs_derr)
+result = Secant.solve(equation)
+print(result)
+assert_equal(result.calculation_result, 1.1257726213960026)
--- numanalysis/nonlineareq/non_linear_equations.py
+++ numanalysis/nonlineareq/non_linear_equations.py
@@ -1,120 +1,170 @@
+from dataclasses import dataclass, field
+from typing import Callable, Type
+
 import numpy as np
-from typing import Callable
 
 
-def fault(lhs: Callable[[float], float], initial_root_candidate: float, left_border: float, right_border: float):
-    if lhs(initial_root_candidate)*lhs(right_border) < 0:
-        return abs(right_border-initial_root_candidate)
-    else:
-        return abs(left_border-initial_root_candidate)
+@dataclass
+class NonLinearEquationResult:
+    calculation_result: float = 0
+    log: list = field(default_factory=list)
+
+    def __str__(self):
+        res = f'\n'.join(self.log)
+        res += f'\nCalculation result is = {self.calculation_result}'
+        return res
+
+    def __repr__(self):
+        return f'res = {self.calculation_result}'
 
 
-def input_check(left_border: float, right_border: float, initial_root_candidate: float):
-    if left_border >= right_border:
-        raise ValueError(
-            f"Invalid boundaries, a={left_border} >= b ={right_border}")
-    if initial_root_candidate < left_border or initial_root_candidate > right_border:
-        raise ValueError(
-            f"Invalid starting approximation, x0={initial_root_candidate} is out of boundaries [{left_border},{right_border}]")
+@dataclass
+class NonLinearEquation:
+    lhs: Callable[[float], float]
+    left_border: float
+    right_border: float
+    initial_root_candidate: float = None
+    lhs_derrivative: Callable[[float], float] = None
+    lhs_derrivative2: Callable[[float], float] = None
+    delta: float = 1e-6
+
+    def __post_init__(self):
+        self.__input_check()
+
+    def __input_check(self):
+        if self.left_border >= self.right_border:
+            raise ValueError(
+                f'Invalid boundaries, left border ={self.left_border} >= right border ={self.right_border}'
+            )
+        if self.initial_root_candidate < self.left_border or self.initial_root_candidate > self.right_border:
+            raise ValueError(
+                f'Invalid starting approximation, initial root candidate={self.initial_root_candidate} is out of boundaries[{self.left_border}, {self.right_border}]'
+            )
+        if self.lhs(self.left_border)*self.lhs(self.right_border) > 0:
+            raise ValueError(
+                f'No root can be found on the segment [{self.left_border}, {self.right_border}]'
+            )
+
+    def initial_approximation_fault(self):
+        return abs(NonLinearEquation.closest_border() - self.initial_root_candidate)
+
+    def closest_border(self):
+        if self.lhs(self.initial_root_candidate) * self.lhs(self.right_border) < 0:
+            return self.right_border
+        return self.left_border
 
 
-def newton(lhs: Callable[[float], float], lhs_derr: Callable[[float], float], lhs_derr_2: Callable[[float], float],
-           left_border: float, right_border: float, initial_root_candidate: float = None, delta: float = 1e-4) -> float:
-    """
-    lhs:Left-hand side of the equation\n
-    lhs_derr: Derrivative of the left - hand side of the equation\n
-    initial_root_candidate: Initial root approximation\n
-    left_border: Left border of the search interval\n
-    right_border: Right border of the search interval\n
-    delta: Tolerated method error\n
-    returns Approximated root of the lhs in the inteval
-    """
-    input_check(left_border, right_border, initial_root_candidate)
-
-    if lhs(left_border)*lhs(right_border) >= 0:
-        raise ValueError(
-            f"No root can be found on [{left_border},{right_border}]")
-
-    if initial_root_candidate is None:
-        if lhs(left_border)*lhs_derr_2(left_border) > 0:
-            initial_root_candidate = left_border
-        elif lhs(right_border)*lhs_derr_2(right_border) > 0:
-            initial_root_candidate = right_border
-        else:
-            initial_root_candidate = (left_border+right_border)/2
-
-    def step(x):
-        return x-lhs(x)/lhs_derr(x)
-
-    xi = initial_root_candidate
-    i = 0
-
-    while abs(step(xi)-xi) > delta or lhs(step(xi)) > delta:
-        i += 1
-        xi = step(xi)
-
-    return xi
+class NonLinearEquationMethod:
+    @classmethod
+    def solve(cls, equation: Type[NonLinearEquation])-> NonLinearEquationResult:
+        raise NotImplementedError()
 
 
-def secant(lhs: Callable[[float], float], left_border: float, right_border: float,
-           initial_root_candidate: float, next_root_candidate: float, delta: float = 1e-6) -> float:
-    """
-    lhs:Left-hand side of the equation\n
-    initial_root_candidate: Initial root approximation\n
-    left_border: Left border of the search interval\n
-    right_border: Right border of the search interval\n
-    delta: Tolerated method error\n
-    returns Approximated root of the lhs in the inteval
-    """
+class Newton(NonLinearEquationMethod):
+    @classmethod
+    def solve(cls, equation: Type[NonLinearEquation])-> NonLinearEquationResult:
+        """
+        lhs: Left-hand side of the equation\n
+        lhs_derr: Derrivative of the left - hand side of the equation\n
+        initial_root_candidate: Initial root approximation\n
+        left_border: Left border of the search interval\n
+        right_border: Right border of the search interval\n
+        delta: Tolerated method error\n
+        returns Approximated root of the lhs in the inteval
+        """
+        cls.__initial_root(equation)
 
-    input_check(left_border, right_border, initial_root_candidate)
-    input_check(left_border, right_border, next_root_candidate)
-    if lhs(left_border)*lhs(right_border) >= 0:
-        raise ValueError(
-            f"No root can be found on [{left_border},{right_border}]")
-    cur, prev = next_root_candidate, initial_root_candidate
+        def step(root_candidate):
+            return root_candidate-equation.lhs(root_candidate)/equation.lhs_derrivative(root_candidate)
 
-    def step(cur):
-        nonlocal prev
-        next = cur-lhs(cur)*(cur-prev)/(lhs(cur)-lhs(prev))
-        prev = cur
-        return next
+        root = equation.initial_root_candidate
+        i = 0
+        result = NonLinearEquationResult()
+        while abs(step(root)-root) > equation.delta or equation.lhs(step(root)) > equation.delta:
+            result.log.append(
+                f'Iteration number = {i}, current approximation = {root}, error = {equation.lhs(root)}'
+            )
+            i += 1
+            root = step(root)
 
-    i = 0
+        result.calculation_result = root
 
-    while abs(cur-prev) > delta or abs(lhs(cur)) > delta:
-        i += 1
-        cur = step(cur)
-    return cur
+        return result
+
+    @classmethod
+    def __initial_root(cls, equation: Type[NonLinearEquation]):
+        if equation.initial_root_candidate is None:
+            if equation.lhs(equation.left_border) * equation.lhs_derrivative2(equation.left_border) > 0:
+                equation.initial_root_candidate = equation.left_border
+            elif equation.lhs(equation.right_border) * equation.lhs_derrivative2(equation.right_border) > 0:
+                equation.initial_root_candidate = equation.right_border
+            else:
+                equation.initial_root_candidate = (
+                    equation.left_border + equation.right_border)/2
 
 
-def relax(lhs: Callable[[float], float], lhs_derr: Callable[[float], float], left_border: float, right_border: float,
-          initial_root_candidate: float,  delta: float = 1e-6) -> float:
-    """
-    lhs:Left-hand side of the equation\n
-    initial_root_candidate: Initial root approximation\n
-    left_border: Left border of the search interval\n
-    right_border: Right border of the search interval\n
-    delta: Tolerated method error\n
-    returns Approximated root of the lhs in the inteval
-    """
-    input_check(left_border, right_border, initial_root_candidate)
-    if lhs(left_border)*lhs(right_border) >= 0:
-        raise ValueError(
-            f"No root can be found on [{left_border},{right_border}]")
-    x_vals = np.linspace(left_border, right_border, min(int(1./delta), 10**5))
-    min_val, max_val = round(np.min(np.abs(lhs_derr(x_vals))), 6), round(
-        np.max(np.abs(lhs_derr(x_vals))), 6)
-    tau = 2./(min_val+max_val)
+class Secant(NonLinearEquationMethod):
+    @classmethod
+    def solve(cls, equation: Type[NonLinearEquation])-> NonLinearEquationResult:
+        """
+        lhs: Left-hand side of the equation\n
+        initial_root_candidate: Initial root approximation\n
+        left_border: Left border of the search interval\n
+        right_border: Right border of the search interval\n
+        delta: Tolerated method error\n
+        returns Approximated root of the lhs in the inteval
+        """
 
-    def phi(x) -> float:
-        return x-np.sign(lhs_derr(x))*tau*lhs(x)
+        x_vals = np.linspace(equation.left_border, equation.right_border, min(
+            int(1./equation.delta), 10**5))
+        min_val, max_val = round(np.min(np.abs(equation.lhs_derrivative(x_vals))), 6), round(
+            np.max(np.abs(equation.lhs_derrivative(x_vals))), 6)
+        tau = 2./(min_val+max_val)
 
-    i = 0
-    xi = initial_root_candidate
+        def step(root_candidate):
+            return root_candidate-np.sign(equation.lhs_derrivative(root_candidate))*tau*equation.lhs(root_candidate)
 
-    while abs(phi(xi)-xi) > delta or lhs(phi(xi)) > delta:
-        i += 1
-        xi = phi(xi)
+        i = 0
+        root = equation.initial_root_candidate
+        result = NonLinearEquationResult()
+        while abs(step(root)-root) > equation.delta or equation.lhs(step(root)) > equation.delta:
+            result.log.append(
+                f'Iteration number = {i}, current approximation = {root}, error = {equation.lhs(root)}'
+            )
+            i += 1
+            root = step(root)
+        result.calculation_result = root
 
-    return xi
+        return result
+
+
+class Relaxation(NonLinearEquationMethod):
+    @classmethod
+    def solve(cls, equation: Type[NonLinearEquation]) -> NonLinearEquationResult:
+        """
+        lhs: Left-hand side of the equation\n
+        initial_root_candidate: Initial root approximation\n
+        left_border: Left border of the search interval\n
+        right_border: Right border of the search interval\n
+        delta: Tolerated method error\n
+        returns Approximated root of the lhs in the inteval
+        """
+
+        root, previous_root = (equation.closest_border(
+        ) + equation.initial_root_candidate)/2, equation.initial_root_candidate
+
+        def step(cur, prev):
+            cur, prev = cur - \
+                equation.lhs(cur) * (cur-prev) / \
+                (equation.lhs(cur) - equation.lhs(prev)), cur
+            return cur, prev
+        i = 0
+        result = NonLinearEquationResult()
+        while abs(root-previous_root) > equation.delta or abs(equation.lhs(root)) > equation.delta:
+            result.log.append(
+                f'Iteration number = {i}, current approximation = {root}, error = {equation.lhs(root)}'
+            )
+            i += 1
+            root, previous_root = step(root, previous_root)
+        result.calculation_result = root
+        return result
