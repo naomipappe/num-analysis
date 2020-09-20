ebbc107cbc2fffea4759f277d3c694827d96bf05
naomipappe <naomipappe.zvieriev@gmail.com>
2020-09-20 11:49:26 +0300

    Started to write doc - strings

--- tests/test_non_linear_equations.py
+++ tests/test_non_linear_equations.py
@@ -1,69 +1,62 @@
-from operator import eq
 import unittest
 from numpy import sin, cos
-from numpy.core.defchararray import equal
-from numpy.core.multiarray import result_type
 from numanalysis.nonlineareq.non_linear_equations import *
 
 
 def lhs(x: float) -> float:
-    return sin(x+2)-x**2+2*x-1
+    return sin(x + 2) - x ** 2 + 2 * x - 1
 
 
-def lhs_derr(x: float) -> float:
-    return -2*x+cos(x+2)+2
+def lhs_der(x: float) -> float:
+    return -2 * x + cos(x + 2) + 2
 
 
-def lhs_derr_2(x: float) -> float:
-    return -sin(x+2)-2
+def lhs_der_2(x: float) -> float:
+    return -sin(x + 2) - 2
 
 
 class TestSecantMethod(unittest.TestCase):
     def test_sine_equation(self):
-
         # Arrange
         left_border = 0.0
         right_border = 0.1
         initial_root_candidate = 0.0
-        next_root_candidate = 0.05
-        ROOT = 0.0606335
+        root = 0.0606335
         equation = NonLinearEquation(
-            lhs, left_border, right_border, initial_root_candidate, lhs_derr)
+            lhs, left_border, right_border, initial_root_candidate, lhs_der)
 
         # Act
         secant_root = Secant.solve(equation)
 
         # Assert
         self.assertAlmostEqual(
-            ROOT, secant_root.calculation_result, delta=1e-6)
+            root, secant_root.calculation_result, delta=1e-6)
 
 
 class TestNewtonsMethod(unittest.TestCase):
     def test_sine_equation(self):
-
         # Arrange
         left_border = 0.0
         right_border = 0.1
         initial_root_candidate = 0.0
-        ROOT = 0.0606335
+        root = 0.0606335
         equation = NonLinearEquation(
-            lhs, left_border, right_border, initial_root_candidate, lhs_derr, lhs_derr_2)
+            lhs, left_border, right_border, initial_root_candidate, lhs_der, lhs_der_2)
         # Act
         newtons_root = Newton.solve(equation)
 
         # Assert
         self.assertAlmostEqual(
-            ROOT, newtons_root.calculation_result, delta=1e-6)
+            root, newtons_root.calculation_result, delta=1e-6)
 
 
 class TestRelaxationMethod(unittest.TestCase):
     def test_sine_equation(self):
-
         # Arrange
         left_border = 0.0
         right_border = 0.1
         initial_root_candidate = 0.0
-        ROOT = 0.0606335
+        root = 0.0606335
         equation = NonLinearEquation(
             lhs, left_border, right_border, initial_root_candidate
         )
@@ -71,4 +64,4 @@ class TestRelaxationMethod(unittest.TestCase):
         relax_root = Relaxation.solve(equation)
 
         # Assert
-        self.assertAlmostEqual(ROOT, relax_root.calculation_result, delta=1e-6)
+        self.assertAlmostEqual(root, relax_root.calculation_result, delta=1e-6)
